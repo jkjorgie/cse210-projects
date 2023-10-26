4a9534572ef49298d0af58e54f3e93b2b9f0a7e5
jkjorgie <jkjorgie@yahoo.com>
2023-10-26 07:58:10 -0600

    added white space

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -7,6 +7,7 @@ class Program
     static void Main(string[] args)
     {
         List<Scripture> scriptures = new List<Scripture>();
+        
         Reference myRef = new Reference("John", 3 ,16, "For God so loved the world that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life.");
         Scripture scripture = new Scripture(myRef);
         scriptures.Add(scripture);
